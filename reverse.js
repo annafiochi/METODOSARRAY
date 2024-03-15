d2d7ff4b478e324bb93a37b518cb5815a4ac6291
annafiochi <annafiochi.tds2.senai@gmail.com>
2024-03-15 16:10:29 -0300

    reverse

---
+++ reverse.js
@@ -0,0 +1,8 @@
+//o reverse muda a ordem dos itens do array (o ultimo se torna o primeiro) sem alterar o array original 
+let arrayfrutas = ['laranja', 'limao', 'morango'];
+console.log('arrayfrutas;', arrayfrutas);
+//aqui e so pra ele "decorar a original"
+let reverso = arrayfrutas.reverse()
+console.log('reverso:', reverso);
+//o esperado é que ele inverta a ordem (ultimo para primeiro)
+console.log('arrayfrutas', arrayfrutas);
