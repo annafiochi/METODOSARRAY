3bcb9dce39443d08026bc93102c47b89a256f76e
annafiochi <annafiochi.tds2.senai@gmail.com>
2024-03-15 18:47:52 -0300

    filter

--- filter.js
+++ filter.js
@@ -1,19 +1,24 @@
 //o filtro ele filtra os itens de acordo com a regra estabelecida 
 //every= apenas fala se esta certo ou nao
 //filter= fala qual palava esta certa
-let palavra = ['linda', 'maravihosa', 'exuberante','extravagante'];
+let palavra = ['linda', 'maravihosa', 'exuberante', 'extravagante'];
 // aqui coloquei pra exibir qual palavra tem menos de 14 caracteres 
-let resultado = palavra.filter((palavra) => palavra.length <14);
+let resultado = palavra.filter((palavra) => palavra.length < 14);
 
 console.log(resultado);
 //esperado que ele exiba 'linda', 'maravihosa', 'exuberante','extravagante'
 
 //////////////////////
+
+//nesse caso eu quero que ele filtre qual dos itens começa com a letra B
 let paises = ["frança", "bolivia", "senegal", "brasil"];
 //agora vamos utilizar o fuction retornapaises que vai ler o primeiro letra de todas as palavras em busca de letra que escolhemos
-function retornapaises (valor){
-    if (valor.charAt(0) =="b")
-return valor;
+//dentro do fuction eu faço a condiçao e uso o "charAt" que vai por cada letra em busca do indice de letra que escolhemos
+function retornapaises(valor) {
+ // nesse caso como quero que seja a primeira letra nosso indice e '0'  
+     if (valor.charAt(0) == "b")
+// utilizo == para explicar que um valor é igual a outro é uma condiçao que precisa ser verdadeira para executar o if
+        return valor;
 }
 let resultado01 = paises.filter(retornapaises);
 console.log(resultado01);
